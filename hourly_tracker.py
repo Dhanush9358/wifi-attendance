6b16ff1c20a6e604923142104615aca311b3f7a0
dhanushdarapureddy9358@gmail.com <dhanushdarapureddy9358@gmail.com>
2025-08-17 21:21:17 +0530

    update local time

--- hourly_tracker.py
+++ hourly_tracker.py
@@ -12,15 +12,19 @@
 #     hourly_check()
 
 import time
-from datetime import datetime
+from datetime import datetime, timezone, timedelta
 import attendance_updater
+
+IST = timezone(timedelta(hours=5, minutes=30))
+
+now = datetime.now(IST)
 
 def hourly_check():
     print("⏰ Starting hourly location check...")
     while True:
-        print(f"\n=== Check started at {datetime.now().strftime('%Y-%m-%d %H:%M:%S')} ===")
+        print(f"\n=== Check started at {now.strftime('%Y-%m-%d %H:%M:%S')} ===")
         attendance_updater.main()
-        print(f"✅ Check complete at {datetime.now().strftime('%Y-%m-%d %H:%M:%S')}. Waiting 60 seconds...\n")
+        print(f"✅ Check complete at {now.strftime('%Y-%m-%d %H:%M:%S')}. Waiting 60 seconds...\n")
         time.sleep(60)  # or any interval you want
 
 if __name__ == "__main__":
